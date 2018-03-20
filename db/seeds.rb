e468eea9edbc7039468cb3a3eaa636f0f3f49a23
Hiroto Fukui <hiroto.fukui822@gmail.com>
2018-03-20 11:29:34 +0900

    Populate initial seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+include GenderEnumerable, ColorEnumerable, ToyCategoryEnumerable
+
+bary = Owner.create name: 'Bary', birthdate: Date.parse('1991-08-22'), gender: GENDER[:male]
+
+goemon = Dog.create owner_id: bary.id, name: 'Goemon', birthdate: Date.parse('2018-08-19'), gender: GENDER[:male], color: COLOR[:red], weight: 15.5, description: 'Smart, yet wonder boy.'
+jigen = Dog.create owner_id: bary.id, name: 'Jigen', birthdate: Date.parse('2020-12-07'), gender: GENDER[:female], color: COLOR[:black], weight: 6.3, description: 'Cheerful, and bouncing girl.'
+
+Toy.create dog_id: goemon.id, name: 'Mesh Ball', category: TOYCATEGORY[:fetch]
+Toy.create dog_id: goemon.id, name: 'Natural Cotton Rope', category: TOYCATEGORY[:pull]
+Toy.create dog_id: jigen.id, name: 'Kong', category: TOYCATEGORY[:think]
