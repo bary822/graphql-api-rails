0590b3cf1e043d24e65fc604eef008859ba92b2a
Hiroto Fukui <hiroto.fukui822@gmail.com>
2018-03-20 10:49:47 +0900

    Dog has_many Toys as FavoriteToys

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,6 +2,7 @@ class Dog < ApplicationRecord
   include GenderEnumerable, ColorEnumerable
 
   belongs_to :owner
+  has_many :favorite_toys, class_name: 'Toy'
 
   enum gender: GENDER
   enum color: COLOR
