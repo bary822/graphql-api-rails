34fd135283d69150ff894489003cc2e9c897de14
Hiroto Fukui <hiroto.fukui822@gmail.com>
2018-04-10 10:56:13 +0900

    Dog has_many Friends through FriendShips

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -3,6 +3,8 @@ class Dog < ApplicationRecord
 
   belongs_to :owner
   has_many :favorite_toys, class_name: 'Toy'
+  has_many :friend_ships
+  has_many :friends, through: :friend_ships, source: :friend
 
   enum gender: GENDER
   enum color: COLOR
--- app/models/friend_ship.rb
+++ app/models/friend_ship.rb
@@ -1,2 +1,4 @@
 class FriendShip < ApplicationRecord
+  belongs_to :dog
+  belongs_to :friend, class_name: 'Dog'
 end
