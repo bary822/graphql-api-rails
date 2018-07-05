db4b23b0b9cf486d619bb6844c46eb669e7f3be8
hfukui <hiroto.fukui822@gmail.com>
2018-07-06 08:21:18 +0900

    Restrict argument to Int for automated validation

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,11 +2,11 @@ class Types::QueryType < Types::BaseObject
   description 'Root level type for Query'
 
   field :owner, Types::OwnerType, 'A human who loves dogs', null: true, method: :find_owner do
-    argument :owner_id, ID, 'ID of the owner', required: true
+    argument :owner_id, Int, 'ID of the owner', required: true
   end
 
   field :dog, Types::DogType, 'The most beloves animal in the world', null: true, method: :find_dog do
-    argument :dog_id, ID, 'ID of the dog', required: true
+    argument :dog_id, Int, 'ID of the dog', required: true
   end
 
   def find_owner(owner_id:)
